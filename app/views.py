6f882c0f19b2607dd3138d4db59ee7a37e86c825
aescudero <adrianescudero1305@gmail.com>
2023-10-27 09:27:42 -0500

    made paths and views for every team page

--- app/views.py
+++ app/views.py
@@ -7,3 +7,19 @@ from django.http.response import HttpResponse
 
 def home_view(request: HttpRequest) -> render:
     return render(request, "home.html")
+
+
+def management_view(request: HttpRequest) -> render:
+    return render(request, "management.html")
+
+
+def procurement_view(request: HttpRequest) -> render:
+    return render(request, "procurement.html")
+
+
+def documentation_view(request: HttpRequest) -> render:
+    return render(request, "documentation.html")
+
+
+def community_view(request: HttpRequest) -> render:
+    return render(request, "community.html")
--- config/urls.py
+++ config/urls.py
@@ -18,17 +18,17 @@ from django.contrib import admin
 from django.urls import path
 from app.views import (
     home_view,
-    # management_view,
-    # procurement_view,
-    # documentation_view,
-    # community_view,
+    management_view,
+    procurement_view,
+    documentation_view,
+    community_view,
 )
 
 urlpatterns = [
     path("", home_view),
-    # path("management/", management_view),
-    # path("procurement/", procurement_view),
-    # path("documentation/", documentation_view),
-    # path("community/", community_view),
+    path("management/", management_view),
+    path("procurement/", procurement_view),
+    path("documentation/", documentation_view),
+    path("community/", community_view),
     path("admin/", admin.site.urls),
 ]
